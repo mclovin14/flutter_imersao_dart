1c25920d509ce0ecd97cc1e653c956d2b01ae99d
Kelven Barbieri <kelven.14barbieri@gmail.com>
2023-08-19 12:48:29 -0300

    Implementa tela de criação de usuário ADM

--- frontend/lib/src/core/restClient/interceptors/auth_interceptor.dart
+++ frontend/lib/src/core/restClient/interceptors/auth_interceptor.dart
@@ -1,18 +1,22 @@
+import 'dart:io';
 
 import 'package:dio/dio.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-
 import '../../constants/local_storage_keys.dart';
+import '../../ui/barbershop_nav_global_key.dart';
 
-class AuthInterceptor extends Interceptor{
+class AuthInterceptor extends Interceptor {
   @override
-  void onRequest(RequestOptions options, RequestInterceptorHandler handler) async {
+  void onRequest(
+      RequestOptions options, RequestInterceptorHandler handler) async {
     final RequestOptions(:headers, :extra) = options;
-    
+
     const String authHeaderKey = 'Authorization';
     headers.remove(authHeaderKey);
-    if(extra case {'DIO_AUTH_KEY': true}){// nesta linha ocorre a mesma coisa de utilizar um containskey e verificar se a key tem o valor true
-      final sp = await  SharedPreferences.getInstance();
+    if (extra case {'DIO_AUTH_KEY': true}) {
+      // nesta linha ocorre a mesma coisa de utilizar um containskey e verificar se a key tem o valor true
+      final sp = await SharedPreferences.getInstance();
       headers.addAll({
         authHeaderKey: 'Bearer ${sp.getString(LocalStorageKeys.accessToken)}'
       });
@@ -20,5 +24,17 @@ class AuthInterceptor extends Interceptor{
     handler.next(options);
 
     super.onRequest(options, handler);
+  }
+
+  @override
+  void onError(DioException err, ErrorInterceptorHandler handler) {
+    final DioException(requestOptions: RequestOptions(:extra), :response) = err;
+
+    if (extra case {'DIO_AUTH_KEY': true}) {
+      if(response != null && response.statusCode == HttpStatus.forbidden){
+        Navigator.of(BarbershopNavGlobalKey.instance.navKey.currentContext!).pushNamedAndRemoveUntil('/auth/login', (route) => false);
+      }
+    }
+    handler.reject(err); //somos obrigados a rejeitar a requisição para que nao fique em looping.
   }
 }
